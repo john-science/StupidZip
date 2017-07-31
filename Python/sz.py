761eb9ea2c4fc8892ec2516c591e79573ba50751
John Stilley <thejollysin@gmail.com>
2017-07-30 19:28:00 -0700

    script placeholder

---
+++ Python/sz.py
@@ -0,0 +1,26 @@
+""" StupidZip
+    
+    This is a utility script to help you compress and uncompress files,
+    without having to memorize all of the annoying flags and options
+    in those tools.
+
+    This is the reference implementation of StupidZip.
+    All future versions must conform to the behaivor in this script.
+    The support for compression utiltities, commandline/flag behavior,
+    and how it handles errors all must match this script.
+    
+    This script is designed for POSIX-style commandlines.
+    
+    This script MUST conform to both the Python v2.x and v3.x standards,
+    to support as many users as possible.
+"""
+import os
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
