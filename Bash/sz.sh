862e604b7e04ff733598588e253b66a718810faf
John Stilley <thejollysin@gmail.com>
2017-08-03 08:06:14 -0700

    #3 adding mirrored help menu

--- Bash/sz.sh
+++ Bash/sz.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # POSIX
 
-echo "WARNING: This is just a placeholder script."
-echo "WARNING" No actions have been taken."
-exit
+# documentation to be printed in usage/help menu
+usage="StupidZip\n
+Usage: sz [-vh] [FILE or DIRECTORY]...\n
+Decompress compressed FILEs or DIRECTORYs in-place, and\n
+tar-gzip uncompressed FILEs or DIRECTORYs in-place.\n
+Optional Arguments:\n
+  -v, --verbose  verbose mode\n
+  -h, --help     give this help\n
+At least one FILE or DIRECTORY path must be provided.\n
+Report bugs to <https://github.com/theJollySin/StupidZip/issues>."
 
+echo -e "WARNING: This is just a placeholder script.\nWARNING" No actions have been taken."
+exit
