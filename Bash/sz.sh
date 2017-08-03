6ab349c99e12cee9ce6f2fe9ade7f2f2d4cfbaf6
John Stilley <thejollysin@gmail.com>
2017-08-03 08:10:52 -0700

    #3 help menu now a function

--- Bash/sz.sh
+++ Bash/sz.sh
@@ -2,15 +2,18 @@
 # POSIX
 
 # documentation to be printed in usage/help menu
-usage="StupidZip\n
-Usage: sz [-vh] [FILE or DIRECTORY]...\n
-Decompress compressed FILEs or DIRECTORYs in-place, and\n
-tar-gzip uncompressed FILEs or DIRECTORYs in-place.\n
-Optional Arguments:\n
-  -v, --verbose  verbose mode\n
-  -h, --help     give this help\n
-At least one FILE or DIRECTORY path must be provided.\n
-Report bugs to <https://github.com/theJollySin/StupidZip/issues>."
+function usage {
+  echo "StupidZip"
+  echo "Usage: sz [-vh] [FILE or DIRECTORY]..."
+  echo "Decompress compressed FILEs or DIRECTORYs in-place, and"
+  echo "tar-gzip uncompressed FILEs or DIRECTORYs in-place."
+  echo "Optional Arguments:"
+  echo "  -v, --verbose  verbose mode"
+  echo "  -h, --help     give this help"
+  echo "At least one FILE or DIRECTORY path must be provided."
+  echo "Report bugs to <https://github.com/theJollySin/StupidZip/issues>."
+}
 
-echo -e "WARNING: This is just a placeholder script.\nWARNING" No actions have been taken."
+echo "WARNING: This is just a placeholder script."
+echo "WARNING" No actions have been taken."
 exit
