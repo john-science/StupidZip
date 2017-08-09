f0304cd34c04b3f16dca8b4108f1ee65acc5d054
John Stilley <thejollysin@gmail.com>
2017-08-09 15:50:58 -0700

    #1 first draft of C version - seems to work

--- C/sz.c
+++ C/sz.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_CMN_LEN 512
+#define MAX_CMN_LEN 1024
 const char *USAGE = "StupidZip\n" \
     "Usage: sz [-vh] [FILE or DIRECTORY]...\n" \
     "Decompress compressed FILEs or DIRECTORYs in-place, and\n" \
@@ -24,26 +24,26 @@ const char *FILE_TYPES[] = {".tar.bz2",
                             ".tbz2",
                             ".tgz",
                             ".zip"};
-const char *DECOMPRESS[] = {"tar xjf path && rm -f path",
-                            "tar xzf path && rm -f path",
-                            "tar xJf path && rm -f path",
-                            "bzip2 -d path",
-                            "gunzip path",
-                            "jar xf path && rm -f path",
-                            "tar xf path && rm -f path",
-                            "tar xjf path && rm -f path",
-                            "tar xzf path && rm -f path",
-                            "unzip path && rm -f path"};
-const char *DECOMPRESS_VERBOSE[] = {"tar xjfv path && rm -f path",
-                                    "tar xzfv path && rm -f path",
-                                    "tar xJfv path && rm -f path",
-                                    "bzip2 -dv path",
-                                    "gunzip -v path",
-                                    "jar xfv path && rm -f path",
-                                    "tar xfv path && rm -f path",
-                                    "tar xjfv path && rm -f path",
-                                    "tar xzfv path && rm -f path",
-                                    "unzip path && rm -f path"};
+const char *DECOMPRESS[] = {"tar xjf ? && rm -f ?",
+                            "tar xzf ? && rm -f ?",
+                            "tar xJf ? && rm -f ?",
+                            "bzip2 -d ?",
+                            "gunzip ?",
+                            "jar xf ? && rm -f ?",
+                            "tar xf ? && rm -f ?",
+                            "tar xjf ? && rm -f ?",
+                            "tar xzf ? && rm -f ?",
+                            "unzip ? && rm -f ?"};
+const char *DECOMPRESS_VERBOSE[] = {"tar xjfv ? && rm -f ?",
+                                    "tar xzfv ? && rm -f ?",
+                                    "tar xJfv ? && rm -f ?",
+                                    "bzip2 -dv ?",
+                                    "gunzip -v ?",
+                                    "jar xfv ? && rm -f ?",
+                                    "tar xfv ? && rm -f ?",
+                                    "tar xjfv ? && rm -f ?",
+                                    "tar xzfv ? && rm -f ?",
+                                    "unzip ? && rm -f ?"};
 const char *COMPRESS = "tar zcf ?.tar.gz ? && rm -rf ?";
 const char *COMPRESS_VERBOSE = "tar zcfv ?.tar.gz ? && rm -rf ?";
 const int NUM_HELP_FLAGS = 4;
@@ -53,11 +53,12 @@ const char *VERBOSE_FLAGS[] = {"-v", "--v", "-verbose", "--verbose"};
 
 
 /**
- * Custom string replacement method, designed for custom (de)compress strings.
+ * Custom string replacement function, designed for custom (de)compress strings.
  * Copy the `from` string to the `to` string, but
  * replace all `?` in `from` with `str`.
  */
-void replace_all_questions(char *str, const char *from, char *to) {
+void replace_all_questions(char *str, const char *from, char *to)
+{
     int s = 0, f = 0, t = 0;
 
     /* loop over characters in from array */
@@ -82,10 +83,40 @@ void replace_all_questions(char *str, const char *from, char *to) {
         }
         f += 1;
     }
+
     /* null terminate the string */
     to[t] = '\0';
 }
 
+
+/**
+ * Custom string length function for null-terminated char arrays.
+ * This exists because we have only short strings and `int` is
+ * a more convenient return type.
+ */
+int str_len(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        i += 1;
+    }
+    return i;
+}
+
+
+/**
+ * If the string ends in a slash, strip that off.
+ * This is meant to fix paths for directories, as opposed to files.
+ */
+void strip_trailing_slash(char *s)
+{
+    char last_char = s[str_len(s) - 1];
+    if (last_char == '/' || last_char == '\\')
+    {
+        s[str_len(s) - 1] = '\0';
+    }
+}
 
 
 /**
@@ -93,7 +124,7 @@ void replace_all_questions(char *str, const char *from, char *to) {
  */
 int main(int argc, char *argv[])
 {
-    int i = 0, f = 0, verbose = 0, verbose_index = -999;
+    int i = 0, j = 0, f = 0, verbose = 0, verbose_index = -999, type_len = 0, path_len = 0, matches = 0;
     char command[MAX_CMN_LEN] = "";
 
     /* make suer that some command is passed to this program */
@@ -122,13 +153,61 @@ int main(int argc, char *argv[])
     /*  placeholder: all this does is parrot the tar-zip incoming files */
     for (i=1; i < argc; i++)
     {
+        /* skip verbose flags; they aren't files */
         if (i == verbose_index)
         {
-            /* skip verbose flags; they aren't files */
             continue;
         }
 
-        /* do the actual tar-gzipping */
+        /* check to see if the file needs to be decompressed */
+        path_len = str_len(argv[i]);
+        for (f=0; f < NUM_FILE_TYPES; f++)
+        {
+            type_len = str_len((char*)FILE_TYPES[f]);
+            if (type_len >= path_len){
+                continue;
+            }
+
+            matches = 0;
+
+            /* look through all the compressed file types, and check for a match */
+            for (j=0; j < type_len; j++)
+            {
+                if (argv[i][path_len - type_len + j] == FILE_TYPES[f][j])
+                {
+                    matches = 1;
+                }
+                else
+                {
+                    matches = 0;
+                    break;
+                }
+            }
+
+            if (matches == 1)
+            {
+                /* do the decompression */
+                if (verbose == 1)
+                {
+                    replace_all_questions(argv[i], DECOMPRESS_VERBOSE[f], command);
+                }
+                else
+                {
+                    replace_all_questions(argv[i], DECOMPRESS[f], command);
+                }
+                system(command);
+                break;
+            }
+        }
+
+        /* If you compressed it, you're done. */
+        if (matches)
+        {
+            continue;
+        }
+
+        /* do the tar-gzipping */
+        strip_trailing_slash(argv[i]);
         if (verbose == 1)
         {
             replace_all_questions(argv[i], COMPRESS_VERBOSE, command);
@@ -140,6 +219,5 @@ int main(int argc, char *argv[])
         system(command);
     }
 
-    fprintf(stdout, "\nWARNING: This C version of StupidZip is not yet implemented.\n\n");
     return 0;
 }
